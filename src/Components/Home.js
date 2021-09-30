4564dcd90b3ded497971719a6d38caed499cc623
Rahul Kumar <vhrahulkumar@gmail.com>
2021-09-30 16:42:30 +0530

    layout change

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,24 +5,32 @@ import "./Home.css";
 const Home = () => {
   return (
     <>
-      <div className="container relative mx-auto mt-56 justify-center items-center">
-        <div className="items-center justify-center flex-row flex-wrap">
-          <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
-            <div className="pr-10">
-              <h1 className="text-white font-semibold text-8xl items-center justify-center flex flex-wrap">
-                <span className="ask">Ask</span>
-                <span className="ai">.ai</span>
-              </h1>
+      <div
+        className="h-screen flex flex-col 
+            items-center justify-center"
+      >
+        <div className="container relative mx-auto justify-center items-center mb-36">
+          <div className="items-center justify-center flex-row flex-wrap">
+            <div className="w-full lg:w-6/12 px-4 ml-auto mr-auto text-center">
+              <div className="pr-10">
+                <h1 className="text-white font-semibold text-8xl items-center justify-center flex flex-wrap">
+                  <span className="ask">Ask</span>
+                  <span className="ai">.ai</span>
+                </h1>
+              </div>
             </div>
           </div>
-        </div>
-        <div className="mt-16 ml-96 pl-64">
-          <Link to="para">
-            <button class="flex items-center pr-5 pl-3 px-2 py-2 ask tracking-wide text-black capitalize transition-colors duration-200 transform bg-red-500 hover:bg-red-600 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80">
-              <img src="https://img.icons8.com/windows/32/000000/long-arrow-right.png" />
-              <span class="mx-1">Let's Get Started</span>
-            </button>
-          </Link>
+          <div
+            className="flex flex-col 
+            items-center justify-center pt-8 pr-8"
+          >
+            <Link to="para">
+              <button class="flex items-center pr-5 pl-3 px-2 py-2 ask tracking-wide text-black capitalize transition-colors duration-200 transform bg-red-500 hover:bg-red-600 focus:outline-none focus:ring focus:ring-indigo-300 focus:ring-opacity-80">
+                <img src="https://img.icons8.com/windows/32/000000/long-arrow-right.png" />
+                <span class="mx-1">Let's Get Started</span>
+              </button>
+            </Link>
+          </div>
         </div>
       </div>
     </>
--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -6,7 +6,10 @@ const Nav = () => {
   return (
     <>
       <nav className="bg-black shadow dark:bg-gray-800 border-b-2">
-        <div className="container flex items-center justify-center p-6 pr-20 mx-auto navfont text-gray-200 text-xl capitalize dark:text-gray-300">
+        <div
+          className="flex flex-row 
+        items-center justify-center p-6 pr-14 mx-auto navfont text-gray-200 text-xl capitalize dark:text-gray-300"
+        >
           <Link
             to="/"
             className="border-b-2 border-transparent  hover:border-red-500 mx-1.5 sm:mx-6"
